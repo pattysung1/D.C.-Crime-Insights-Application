64e6abccfc542dfa714f15eae86275dfa359ca72
kflin <kflin@vt.edu>
2024-10-02 22:07:19 -0400

    Update: frontend template

--- src/pages/CrimeMap.js
+++ src/pages/CrimeMap.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Filter from '../components/Filter/Filter'; // 引入 Filter 組件
 // import '../styles/CrimeMap.css'; // 假設你有相關樣式
-
+import CrimeMapComponent from '../components/CrimeMapComponent';
 const CrimeMap = ({ filters, handleFilterChange, handleCrimeTypeChange, handleZoneChange }) => {
     return (
         <div className="crime-map-container">
@@ -15,6 +15,8 @@ const CrimeMap = ({ filters, handleFilterChange, handleCrimeTypeChange, handleZo
             {/* 這裡可以放你的地圖展示部分 */}
             <div className="map-display">
                 {/* 地圖的代碼... */}
+                <p>This is the CrimeMap page content.</p>
+                <CrimeMapComponent />
             </div>
         </div>
     );
