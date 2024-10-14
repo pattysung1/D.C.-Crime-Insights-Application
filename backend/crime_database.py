b851b4cd63bedddcadc00aa886888a88e2be8321
alexmarrerovt <alexmarrero10@vt.edu>
2024-10-14 01:26:35 -0400

    created the database set-up file and created an example graph using mysql-connector on the crime Prediction page

--- backend/crime_database.py
+++ backend/crime_database.py
@@ -1,6 +1,9 @@
 # Create Tables for Crime Database
 
 import mysql.connector
+from datetime import datetime
+import pandas as pd
+import numpy as np
 
 # Creating the connection
 def establish_connection():
@@ -77,8 +80,118 @@ def create_tables():
         print("Tables created successfully.")
 
 
-def populate_tables():
-    '''Populate the tables'''
-    pass
+def insert_into_report_time(row, cursor):
+    '''Populate report_time table'''
 
+    query = """
+    INSERT INTO report_time (ccn, report_date_time, shift)
+    VALUES (%s, %s, %s)
+    """
+    ccn = row['CCN']
+    report_date_time = datetime.strptime(row['REPORT_DAT'], '%m/%d/%Y, %I:%M:%S %p') if row['REPORT_DAT'] else None
+    shift = row['SHIFT']
+    cursor.execute(query, (ccn, report_date_time, shift))
+
+
+def insert_into_report_location(row, cursor):
+    '''Populate report_location table'''
+
+    query = """
+    INSERT INTO report_location (ccn, longitude, latitude, ward, neighborhood_clusters, anc, psa, voting_precinct)
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+    """
+    ccn = row['CCN']
+    longitude = row['LONGITUDE']
+    latitude = row['LATITUDE']
+    ward = row['WARD']
+    neighborhood_clusters = row['NEIGHBORHOOD_CLUSTER']
+    anc = row['ANC']
+    psa = row['PSA']
+    voting_precinct = row['VOTING_PRECINCT']
+    cursor.execute(query, (ccn, longitude, latitude, ward, neighborhood_clusters, anc, psa, voting_precinct))
+
+
+def insert_into_offense_and_method(row, cursor):
+    '''Populate offense_and_method table'''
+
+    query = """
+    INSERT IGNORE INTO offense_and_method (ccn, offense, method, offense_group, ucr_rank)
+    VALUES (%s, %s, %s, %s, %s)
+    """
+    ccn = row['CCN']
+    offense = row['OFFENSE']
+    method = row['METHOD']
+    offense_group = row['offensegroup']
+    ucr_rank = row['ucr-rank']
+    cursor.execute(query, (ccn, offense, method, offense_group, ucr_rank))
+
+
+def populate_tables():
+    '''Calls functions to populate all of the tables'''
+
+    # create cursor if there is a connection
+    if conn != None:
+        cursor = conn.cursor()
+
+        # Get csv (path will change based on users pc)
+        csv_file_path = 'C:\\Users\marre\Downloads\Capstone_CS_5934\DC_Crime_Data_Past_5_Years.csv'
+        data = pd.read_csv(csv_file_path)
+
+        # data cleaning (replacing NaN with None and dropping any duplicate cases)
+        data = data.replace({np.nan: None})
+        data = data.drop_duplicates(subset=['CCN'])
+
+        print ('inserting data')
+
+        # Populate tables for each row
+        count = 0
+        for index, row in data.iterrows():
+            insert_into_report_time(row, cursor)
+            insert_into_report_location(row, cursor)
+            insert_into_offense_and_method(row, cursor)
+            if count % 1000 == 0:
+                print("current at row:", count)
+            count += 1
+
+        print ('finished inserting data')
+
+        # commit changes
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+
+def delete_from_all_tables():
+    '''Deletes everything from all of the tables'''
+    if conn != None:
+        cursor = conn.cursor()
+
+        cursor.execute("DELETE FROM report_location;")
+        cursor.execute("DELETE FROM offense_and_method;")
+        cursor.execute("DELETE FROM report_time;")
+
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+        print("All rows deleted from all tables successfully.")
+
+
+
+def testing_database():
+    '''Checking to make sure things imported correctly'''
+    if conn != None:
+        cursor = conn.cursor()
+
+        cursor.execute("select * from report_time where ccn = 24070106")
+
+        print(cursor.fetchall())
+
+
+
+
+'''Function calls'''
 # create_tables()
+# populate_tables()
+# delete_from_all_tables()
+# testing_database()
