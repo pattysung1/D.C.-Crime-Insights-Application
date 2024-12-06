b0fbe845495b5b3942bc21271cf0e97340b6ac77
kflin <kflin@vt.edu>
2024-12-06 17:41:40 -0500

    Update: display safety route only and add marker

--- src/components/SafeRoutingComponent.js
+++ src/components/SafeRoutingComponent.js
@@ -1,435 +1,28 @@
-// import React, { useEffect } from "react";
-// import { MapContainer, TileLayer, Marker, Popup, Polyline, useMap } from "react-leaflet";
-// import L from "leaflet";
-// import "leaflet/dist/leaflet.css";
-
-// // Custom icons for start and end points
-// const greenIcon = new L.Icon({
-//   iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png",
-//   iconSize: [25, 41],
-//   iconAnchor: [12, 41],
-//   popupAnchor: [1, -34],
-//   shadowSize: [41, 41],
-// });
-
-// const SafeRoutingComponent = ({ route, isLoading, error }) => {
-//   // Fit map bounds to the route
-//   const MapBounds = () => {
-//     const map = useMap();
-
-//     useEffect(() => {
-//       if (route && route.length > 0) {
-//         const bounds = L.latLngBounds(route.map((point) => [point.lat, point.lng]));
-//         map.fitBounds(bounds);
-//       }
-//     }, [route, map]);
-
-//     return null;
-//   };
-
-//   // Default map center and zoom level
-//   const defaultCenter = [38.9072, -77.0369]; // Washington, DC
-//   const defaultZoom = 13;
-
-//   return (
-//     <MapContainer
-//       center={defaultCenter}
-//       zoom={defaultZoom}
-//       style={{ height: "600px", width: "100%" }}
-//     >
-//       <TileLayer
-//         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-//         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-//       />
-
-//       {/* Add dynamic map bounds */}
-//       {route && route.length > 0 && <MapBounds />}
-
-//       {/* Route */}
-//       {route && route.length > 0 && (
-//         <Polyline
-//           positions={route.map((point) => [point.lat, point.lng])}
-//           color="blue"
-//           weight={4}
-//           dashArray="5, 10" // Makes the route a dashed line
-//         />
-//       )}
-
-//       {/* Start Point */}
-//       {route && route.length > 0 && (
-//         <Marker position={[route[0].lat, route[0].lng]} icon={greenIcon}>
-//           <Popup>Start Point</Popup>
-//         </Marker>
-//       )}
-
-//       {/* End Point */}
-//       {route && route.length > 0 && (
-//         <Marker position={[route[route.length - 1].lat, route[route.length - 1].lng]} icon={greenIcon}>
-//           <Popup>Destination</Popup>
-//         </Marker>
-//       )}
-
-//       {/* Default map message */}
-//       {!route && !isLoading && !error && (
-//         <div className="default-map-message">
-//           <p style={{ textAlign: "center", marginTop: "20px" }}>
-//             Enter a start and destination to find a safe route.
-//           </p>
-//         </div>
-//       )}
-//     </MapContainer>
-//   );
-// };
-
-// export default SafeRoutingComponent;
-// -------------------------------------mmmmm===========================================================================================
-// import React, { useEffect } from "react";
-// import { MapContainer, TileLayer, Marker, Popup, Polyline, useMap } from "react-leaflet";
-// import L from "leaflet";
-// import "leaflet/dist/leaflet.css";
-
-// // Custom icons for start and end points
-// const greenIcon = new L.Icon({
-//   iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png",
-//   iconSize: [25, 41],
-//   iconAnchor: [12, 41],
-//   popupAnchor: [1, -34],
-//   shadowSize: [41, 41],
-// });
-
-// const SafeRoutingComponent = ({ safeRoute, regularRoute, isLoading, error }) => {
-//   const MapBounds = () => {
-//     const map = useMap();
-
-//     useEffect(() => {
-//       if ((safeRoute && safeRoute.length > 0) || (regularRoute && regularRoute.length > 0)) {
-//         const bounds = L.latLngBounds([
-//           ...(safeRoute || []).map((point) => [point.lat, point.lng]),
-//           ...(regularRoute || []).map((point) => [point.lat, point.lng]),
-//         ]);
-//         map.fitBounds(bounds);
-//       }
-//     }, [safeRoute, regularRoute, map]);
-
-//     return null;
-//   };
-
-//   const defaultCenter = [38.9072, -77.0369]; // Washington, DC
-//   const defaultZoom = 13;
-
-//   return (
-//     <MapContainer center={defaultCenter} zoom={defaultZoom} style={{ height: "600px", width: "100%" }}>
-//       <TileLayer
-//         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-//         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-//       />
-
-//       <MapBounds />
-
-//       {/* Safe Route */}
-//       {safeRoute && safeRoute.length > 0 && (
-//         <Polyline
-//           positions={safeRoute.map((point) => [point.lat, point.lng])}
-//           color="blue"
-//           weight={4}
-//           dashArray="5, 10"
-//         />
-//       )}
-
-//       {/* Regular Route */}
-//       {regularRoute && regularRoute.length > 0 && (
-//         <Polyline
-//           positions={regularRoute.map((point) => [point.lat, point.lng])}
-//           color="green"
-//           weight={4}
-//         />
-//       )}
-
-//       {/* Start and End Markers */}
-//       {(safeRoute || regularRoute) && (
-//         <>
-//           <Marker
-//             position={[safeRoute?.[0]?.lat || regularRoute?.[0]?.lat, safeRoute?.[0]?.lng || regularRoute?.[0]?.lng]}
-//             icon={greenIcon}
-//           >
-//             <Popup>Start Point</Popup>
-//           </Marker>
-//           <Marker
-//             position={[
-//               safeRoute?.[safeRoute.length - 1]?.lat || regularRoute?.[regularRoute.length - 1]?.lat,
-//               safeRoute?.[safeRoute.length - 1]?.lng || regularRoute?.[regularRoute.length - 1]?.lng,
-//             ]}
-//             icon={greenIcon}
-//           >
-//             <Popup>Destination</Popup>
-//           </Marker>
-//         </>
-//       )}
-
-//       {!safeRoute && !regularRoute && !isLoading && !error && (
-//         <div className="default-map-message">
-//           <p style={{ textAlign: "center", marginTop: "20px" }}>
-//             Enter a start and destination to find routes.
-//           </p>
-//         </div>
-//       )}
-//     </MapContainer>
-//   );
-// };
-
-// export default SafeRoutingComponent;
-
-// =======
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, { useEffect } from "react";
-// import { MapContainer, TileLayer, Marker, Popup, Polyline, useMap } from "react-leaflet";
-// import L from "leaflet";
-// import "leaflet/dist/leaflet.css";
-
-// // Custom icons for start and end points
-// const greenIcon = new L.Icon({
-//   iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png",
-//   iconSize: [25, 41],
-//   iconAnchor: [12, 41],
-//   popupAnchor: [1, -34],
-//   shadowSize: [41, 41],
-// });
-
-// const SafeRoutingComponent = ({ safeRoute, regularRoute, regularRouteColor, isLoading, error }) => {
-//   const MapBounds = () => {
-//     const map = useMap();
-
-//     useEffect(() => {
-//       if ((safeRoute && safeRoute.length > 0) || (regularRoute && regularRoute.length > 0)) {
-//         const bounds = L.latLngBounds([
-//           ...(safeRoute || []).map((point) => [point.lat, point.lng]),
-//           ...(regularRoute || []).map((point) => [point.lat, point.lng]),
-//         ]);
-//         map.fitBounds(bounds);
-//       }
-//     }, [safeRoute, regularRoute, map]);
-
-//     return null;
-//   };
-
-//   const defaultCenter = [38.9072, -77.0369]; // Washington, DC
-//   const defaultZoom = 13;
-
-//   return (
-//     <MapContainer center={defaultCenter} zoom={defaultZoom} style={{ height: "600px", width: "100%" }}>
-//       <TileLayer
-//         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-//         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-//       />
-
-//       <MapBounds />
-
-//       {/* Safe Route */}
-//       {safeRoute && safeRoute.length > 0 && (
-//         <Polyline
-//           positions={safeRoute.map((point) => [point.lat, point.lng])}
-//           color="blue"
-//           weight={4}
-//           dashArray="5, 10"
-//         />
-//       )}
-
-//       {/* Regular Route */}
-//       {regularRoute && regularRoute.length > 0 && (
-//         <Polyline
-//           positions={regularRoute.map((point) => [point.lat, point.lng])}
-//           color={regularRouteColor || "red"} // Use dynamic color from backend or fallback to red
-//           weight={4}
-//         />
-//       )}
-
-//       {/* Start and End Markers */}
-//       {(safeRoute || regularRoute) && (
-//         <>
-//           <Marker
-//             position={[safeRoute?.[0]?.lat || regularRoute?.[0]?.lat, safeRoute?.[0]?.lng || regularRoute?.[0]?.lng]}
-//             icon={greenIcon}
-//           >
-//             <Popup>Start Point</Popup>
-//           </Marker>
-//           <Marker
-//             position={[
-//               safeRoute?.[safeRoute.length - 1]?.lat || regularRoute?.[regularRoute.length - 1]?.lat,
-//               safeRoute?.[safeRoute.length - 1]?.lng || regularRoute?.[regularRoute.length - 1]?.lng,
-//             ]}
-//             icon={greenIcon}
-//           >
-//             <Popup>Destination</Popup>
-//           </Marker>
-//         </>
-//       )}
-
-//       {!safeRoute && !regularRoute && !isLoading && !error && (
-//         <div className="default-map-message">
-//           <p style={{ textAlign: "center", marginTop: "20px" }}>
-//             Enter a start and destination to find routes.
-//           </p>
-//         </div>
-//       )}
-//     </MapContainer>
-//   );
-// };
-
-// export default SafeRoutingComponent;
-
-
-
-// ===
-
-// import React, { useEffect } from "react";
-// import { MapContainer, TileLayer, Polyline, useMap } from "react-leaflet";
-// import "leaflet/dist/leaflet.css";
-
-// const SafeRoutingComponent = ({ safeRoute, regularRoute }) => {
-//   const MapBounds = () => {
-//     const map = useMap();
-
-//     useEffect(() => {
-//       if ((safeRoute && safeRoute.length > 0) || (regularRoute && regularRoute.length > 0)) {
-//         const bounds = L.latLngBounds([
-//           ...(safeRoute || []).map((point) => [point.lat, point.lng]),
-//           ...(regularRoute || []).map((point) => [point.lat, point.lng]),
-//         ]);
-//         map.fitBounds(bounds);
-//       }
-//     }, [safeRoute, regularRoute, map]);
-
-//     return null;
-//   };
-
-//   const defaultCenter = [38.8977, -77.0365]; // Center map on Washington, DC
-//   const defaultZoom = 13;
-
-//   return (
-//     <MapContainer center={defaultCenter} zoom={defaultZoom} style={{ height: "600px", width: "100%" }}>
-//       <TileLayer
-//         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-//         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-//       />
-
-//       <MapBounds />
-
-//       {/* Render Safe Route */}
-//       {/* {safeRoute && safeRoute.length > 0 && (
-//         <Polyline
-//           positions={safeRoute.map((point) => [point.lat, point.lng])}
-//           color="green" // Safe route in green
-//           weight={4}
-//           dashArray="10, 5" // Dashed line for distinction
-//         />
-//       )} */}
-//       {safeRoute && safeRoute.length > 0 && (
-//         <Polyline
-//           positions={safeRoute.map((point) => [point.lat, point.lng])}
-//           color="green" // Safe route in green
-//           weight={4}
-//           dashArray="10, 5" // Dashed line for distinction
-//         />
-//       )}
-
-//       {/* Render Regular Route
-//       {regularRoute && regularRoute.length > 0 && (
-//         <Polyline
-//           positions={regularRoute.map((point) => [point.lat, point.lng])}
-//           color="red" // Regular route in red
-//           weight={4}
-//         />
-//       )} */}
-//       {regularRoute && regularRoute.length > 0 && (
-//         <Polyline
-//           positions={regularRoute.map((point) => [point.lat, point.lng])}
-//           color="red" // Regular route in red
-//           weight={4}
-//           opacity={0.7} // Add transparency
-//         />
-//       )}
-//     </MapContainer>
-//   );
-// };
-
-// export default SafeRoutingComponent;
-
-
-// import React, { useEffect } from "react";
-// import { MapContainer, TileLayer, Polyline, useMap } from "react-leaflet";
-// import "leaflet/dist/leaflet.css";
-
-// const SafeRoutingComponent = ({ safeRoute }) => {
-//   const MapBounds = () => {
-//     const map = useMap();
-
-//     useEffect(() => {
-//       if (safeRoute && safeRoute.length > 0) {
-//         const bounds = L.latLngBounds(safeRoute.map((point) => [point.lat, point.lng]));
-//         map.fitBounds(bounds);
-//       }
-//     }, [safeRoute, map]);
-
-//     return null;
-//   };
-
-//   const defaultCenter = [38.8977, -77.0365]; // Center map on Washington, DC
-//   const defaultZoom = 13;
-
-//   return (
-//     <MapContainer center={defaultCenter} zoom={defaultZoom} style={{ height: "600px", width: "100%" }}>
-//       <TileLayer
-//         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-//         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-//       />
-
-//       <MapBounds />
-
-//       {/* Render Only Safe Route */}
-//       {safeRoute && safeRoute.length > 0 && (
-//         <Polyline
-//           positions={safeRoute.map((point) => [point.lat, point.lng])}
-//           color="green" // Safe route in green
-//           weight={5}
-//           dashArray="10, 5" // Dashed pattern for distinction
-//         />
-//       )}
-//     </MapContainer>
-//   );
-// };
-
-// export default SafeRoutingComponent;
-
 import React, { useEffect } from "react";
-import { MapContainer, TileLayer, Polyline, useMap } from "react-leaflet";
+import { MapContainer, TileLayer, Polyline, Marker, Popup, useMap } from "react-leaflet";
+import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 
-const SafeRoutingComponent = ({ safeRoute, regularRoute }) => {
+// Custom icons for start and end points
+const greenIcon = new L.Icon({
+  iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
+
+const SafeRoutingComponent = ({ safeRoute = [], regularRoute = [] }) => {
+  // Component to adjust the map bounds based on the routes
   const MapBounds = () => {
     const map = useMap();
 
     useEffect(() => {
-      if ((safeRoute && safeRoute.length > 0) || (regularRoute && regularRoute.length > 0)) {
+      // If there are routes, fit the map to their bounds
+      if (safeRoute.length > 0 || regularRoute.length > 0) {
         const bounds = L.latLngBounds([
-          ...(safeRoute || []).map((point) => [point.lat, point.lng]),
-          ...(regularRoute || []).map((point) => [point.lat, point.lng]),
+          ...safeRoute.map((point) => [point.lat, point.lng]),
+          ...regularRoute.map((point) => [point.lat, point.lng]),
         ]);
         map.fitBounds(bounds);
       }
@@ -438,36 +31,66 @@ const SafeRoutingComponent = ({ safeRoute, regularRoute }) => {
     return null;
   };
 
-  const defaultCenter = [38.8977, -77.0365]; // Center map on Washington, DC
+  // Default map center and zoom level (Washington, DC)
+  const defaultCenter = [38.8977, -77.0365];
   const defaultZoom = 13;
+
+  // Extract the start and end points from the safe route
+  const startPoint = safeRoute.length > 0 ? safeRoute[0] : null;
+  const endPoint = safeRoute.length > 0 ? safeRoute[safeRoute.length - 1] : null;
 
   return (
     <MapContainer center={defaultCenter} zoom={defaultZoom} style={{ height: "600px", width: "100%" }}>
       <TileLayer
+        // Use OpenStreetMap tiles for the map
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
 
+      {/* Adjust map bounds based on the routes */}
       <MapBounds />
 
-      {/* Safe Route */}
-      {safeRoute && safeRoute.length > 0 && (
+      {/* Marker for the starting point */}
+      {startPoint && (
+        <Marker position={[startPoint.lat, startPoint.lng]} icon={greenIcon}>
+          <Popup>Start</Popup>
+        </Marker>
+      )}
+
+      {/* Marker for the destination point */}
+      {endPoint && (
+        <Marker position={[endPoint.lat, endPoint.lng]} icon={greenIcon}>
+          <Popup>Destination</Popup>
+        </Marker>
+      )}
+
+      {/* Polyline for the safe route */}
+      {safeRoute.length > 0 && (
         <Polyline
           positions={safeRoute.map((point) => [point.lat, point.lng])}
-          color="green" // Safe route in green
-          weight={5}
+          color="green" // Green for the safe route
+          weight={5} // Line thickness
           dashArray="10, 5" // Dashed pattern for distinction
         />
       )}
 
-      {/* Regular Route */}
-      {regularRoute && regularRoute.length > 0 && (
+      {/* Polyline for the regular route */}
+      {/* {regularRoute.length > 0 && (
         <Polyline
           positions={regularRoute.map((point) => [point.lat, point.lng])}
-          color="red" // Regular route in red
-          weight={4}
-          opacity={0.7} // Add transparency
+          color="red" // Red for the regular route
+          weight={4} // Line thickness
+          opacity={0.7} // Semi-transparent for visibility
         />
+      )} */}
+
+      {/* Default map message */}
+      {!safeRoute.length && !regularRoute.length && (
+        <div className="default-map-message">
+          <p style={{ textAlign: "center", marginTop: "20px" }}>
+            Enter a start and destination to find routes.
+          </p>
+        </div>
       )}
     </MapContainer>
   );
